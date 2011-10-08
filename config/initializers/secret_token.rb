3391659a2828e299cf039a87069c18218d3050fd
yarka <maksbasket@yandex.ru>
2011-10-08 12:59:24 +0400

    Formtastic without localization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '2ee7ac4d00c0b0a3a8ee3340dbc0606a39c8f52e1a7800e6b1bfd196fb598f120ab57a536a6799937f32ea6ce2b122998dcbe2c305ce473a0e39cf2c4f6a0448'
